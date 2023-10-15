ff95ec79f11c7367b8c82a3a27135ed5bb10ca70
Rangel Reale <rangelspam@gmail.com>
2023-10-15 10:57:23 -0300

    internal

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,8 +4,13 @@ import (
 	"context"
 
 	"github.com/RangelReale/debefix-sample-app/internal/entity"
+	"github.com/google/uuid"
 )
 
 type TagStorage interface {
-	GetTags(ctx context.Context, filter *entity.TagsFilter) ([]entity.Tag, error)
+	GetTags(ctx context.Context, filter entity.TagsFilter) ([]entity.Tag, error)
+	GetTagByID(ctx context.Context, tagID uuid.UUID) (entity.Tag, error)
+	AddTag(ctx context.Context, tag entity.Tag) error
+	UpdateTagByID(ctx context.Context, tagID uuid.UUID, tag entity.Tag) error
+	DeleteTagByID(ctx context.Context, tagID uuid.UUID) error
 }
