deb0d1b6515c55a37a6db39cd48eda3c45680e47
Rangel Reale <rangelspam@gmail.com>
2023-10-20 11:12:03 -0300

    user db

--- internal/infrastructure/databasegorm/dbmodel/user.go
+++ internal/infrastructure/databasegorm/dbmodel/user.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/rrgmc/debefix-sample-app/internal/domain/model"
 )
 
 // data
@@ -19,4 +20,34 @@ type User struct {
 
 func (User) TableName() string {
 	return "users"
+}
+
+func (t User) ToEntity() model.User {
+	return model.User{
+		UserID:    t.UserID,
+		Name:      t.Name,
+		Email:     t.Email,
+		CountryID: t.CountryID,
+		CreatedAt: t.CreatedAt,
+		UpdatedAt: t.UpdatedAt,
+	}
+}
+
+func UserListToEntity(list []User) []model.User {
+	var ret []model.User
+	for _, item := range list {
+		ret = append(ret, item.ToEntity())
+	}
+	return ret
+}
+
+func UserFromEntity(item model.User) User {
+	return User{
+		UserID:    item.UserID,
+		Name:      item.Name,
+		Email:     item.Email,
+		CountryID: item.CountryID,
+		CreatedAt: item.CreatedAt,
+		UpdatedAt: item.UpdatedAt,
+	}
 }
