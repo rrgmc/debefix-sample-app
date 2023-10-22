558023fcfab89a61fce88a1337ba9a934dac2b14
Rangel Reale <rangelspam@gmail.com>
2023-10-22 09:47:39 -0300

    use string file provider

--- internal/infrastructure/database/testutils/fixtures/util.go
+++ internal/infrastructure/database/testutils/fixtures/util.go
@@ -1,9 +1,6 @@
 package fixtures
 
 import (
-	"fmt"
-	"testing/fstest"
-
 	"github.com/rrgmc/debefix"
 )
 
@@ -13,12 +10,7 @@ func MergeData(source *debefix.Data, mergeData []string) (*debefix.Data, error) 
 		return nil, err
 	}
 
-	fs := fstest.MapFS{}
-	for idx, data := range mergeData {
-		fs[fmt.Sprintf("file_%02d.dbf.yaml", idx)] = &fstest.MapFile{Data: []byte(data)}
-	}
-
-	newData, err = debefix.Load(debefix.NewFSFileProvider(fs), debefix.WithLoadInitialData(newData))
+	newData, err = debefix.Load(debefix.NewStringFileProvider(mergeData), debefix.WithLoadInitialData(newData))
 	if err != nil {
 		return nil, err
 	}
